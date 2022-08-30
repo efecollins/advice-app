aa1b2e742c5413c7110f6f10ccd81c039194cfb0
efecollins <efejamb@gmail.com>
2022-08-30 10:32:51 +0100

    catch fetch error

--- js/index.js
+++ js/index.js
@@ -3,12 +3,14 @@ let dice = document.querySelector(".dice");
 // store async function in a variable
 const getAdvice = async () => {
     // get request through fetch
-    const response = await fetch('https://api.adviceslip.com/advice');
+    const response = await fetch('https://api.adviceslip.com/advice').catch((error) => {
+        console.error(error);
+    });
     // parse json
     const adviceJSON = await response.json();
     // console.log(adviceJSON);
     document.querySelector('.id').innerHTML = adviceJSON.slip.id;
-    document.querySelector('.quote').innerHTML = '"'+ adviceJSON.slip.advice +'"';
+    document.querySelector('.quote').innerHTML = '"' + adviceJSON.slip.advice + '"';
 }
 
 dice.addEventListener("click", getAdvice);
